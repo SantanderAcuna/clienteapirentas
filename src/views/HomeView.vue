96c2406a84c39d0b81fbbdc60dde2a50e2aafb3c
Jose Acuña <santanderjose19@gmail.com>
2024-11-01 15:49:23 -0500

    Cnfiguracion de la tabla para klistar los datos

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,49 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <table class="table table-bordered">
+    <thead>
+      <tr>
+        <th scope="col">Id</th>
+        <th scope="col">Nombre</th>
+        <th scope="col">Ref catastral</th>
+        <th scope="col">Correo</th>
+        <th scope="row">Acciones</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <th scope="row">1</th>
+        <td>Mark</td>
+        <td>2505222222</td>
+        <td>prueba@gmail.com</td>
+        <a class="btn btn-Warning" href="#" role="button">Editar</a>
+        <button type="submit" class="btn-danger">Eliminar</button>
+        
+        
+      </tr>
+      <tr>
+        <th scope="row">2</th>
+        <td>Mark</td>
+        <td>2505222223</td>
+        <td>prueba2@gmail.com</td>
+        <button type="submit" class="btn btn-warning">Editar</button>
+        <button type="submit" class="btn-danger">Eliminar</button>
+      </tr>
+      <tr>
+        <th scope="row">3</th>
+        <td>Mark</td>
+        <td>2505222224</td>
+        <td>prueba3@gmail.com</td>
+        <button type="submit" class="btn-warning">Editar</button>
+        <button type="submit" class="btn-danger">Eliminar</button>
+      </tr>
+  
+    </tbody>
+  </table>
+
+
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
-export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
-  }
-}
+
 </script>
