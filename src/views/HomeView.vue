dd57bb9ebe6ac70269ac3b6777437906246032e2
Jose Acuña <santanderjose19@gmail.com>
2024-11-01 17:58:29 -0500

    agregando logica al front

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,49 +1,53 @@
 <template>
-  <table class="table table-bordered">
-    <thead>
-      <tr>
-        <th scope="col">Id</th>
-        <th scope="col">Nombre</th>
-        <th scope="col">Ref catastral</th>
-        <th scope="col">Correo</th>
-        <th scope="row">Acciones</th>
-      </tr>
-    </thead>
-    <tbody>
-      <tr>
-        <th scope="row">1</th>
-        <td>Mark</td>
-        <td>2505222222</td>
-        <td>prueba@gmail.com</td>
-        <a class="btn btn-Warning" href="#" role="button">Editar</a>
-        <button type="submit" class="btn-danger">Eliminar</button>
-        
-        
-      </tr>
-      <tr>
-        <th scope="row">2</th>
-        <td>Mark</td>
-        <td>2505222223</td>
-        <td>prueba2@gmail.com</td>
-        <button type="submit" class="btn btn-warning">Editar</button>
-        <button type="submit" class="btn-danger">Eliminar</button>
-      </tr>
-      <tr>
-        <th scope="row">3</th>
-        <td>Mark</td>
-        <td>2505222224</td>
-        <td>prueba3@gmail.com</td>
-        <button type="submit" class="btn-warning">Editar</button>
-        <button type="submit" class="btn-danger">Eliminar</button>
-      </tr>
-  
-    </tbody>
-  </table>
+  <div class="row pt-3">
+    <div class="col-lg-8 offset-lg-2">
+      <div class="table-responsive">
+        <table class="table table-bordered table-hover">
+          <thead>
+            <tr>
+              <th scope="col">Id</th>
+              <th scope="col">Nombre</th>
+              <th scope="col">Ref catastral</th>
+              <th scope="col">Correo</th>
+              <th scope="row">Acciones</th>
+            </tr>
+          </thead>
+          <tbody class="table-group-dividier" id="contenido">
+            <tr v-if="this.cargando">
+              <td colspan="6">
+                <h3>Cargando</h3>
+              </td>
+            </tr>
+            <tr v-else>
+
+            </tr>
+
+          </tbody>
+        </table>
+
+      </div>
+
+    </div>
+
+  </div>
+
 
 
 </template>
 
 <script>
+import axios from 'axios';
+export default {
 
+  data() {
+
+    return {
+      contribuyennte: null,
+      cargando: true,
+
+    }
+  }
+
+}
 
 </script>
